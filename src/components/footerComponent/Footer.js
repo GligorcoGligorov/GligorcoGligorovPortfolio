f5c626d56cb9fe9f65df31fc37fa768500ca75cd
Глигоров Глигорчо <gligorcho.gligorov@students.finki.ukim.mk>
2024-01-31 00:58:41 +0100

    Add mail link in footer

--- src/components/footerComponent/Footer.js
+++ src/components/footerComponent/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <div class="social-footer-icons">
             <div><a href="https://www.linkedin.com/in/gligorco-gligorov-11b282289//"><i class="fa fa-linkedin" aria-hidden="true"></i></a></div>
             <div><a href="https://github.com/GligorcoGligorov"><i class="fa fa-github" aria-hidden="true"></i></a></div>
-            <div><a href=""><i class="fa fa-envelope" aria-hidden="true"></i><span id="mail">ggligorov1709@yahoo.com</span></a></div>
+            <div><a href="mailto:ggligorov1709@yahoo.com"><i class="fa fa-envelope" aria-hidden="true"></i><span id="mail">ggligorov1709@yahoo.com</span></a></div>
         </div>
       </footer>
     );
