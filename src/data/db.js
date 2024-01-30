f29945a5cd100779e9a634b5e5a6ddd982e3555b
Глигоров Глигорчо <gligorcho.gligorov@students.finki.ukim.mk>
2024-01-31 00:58:02 +0100

    Add db data as importable js file and adjust useFetch.js to use this data

---
+++ src/data/db.js
@@ -0,0 +1,102 @@
+export default {
+    "projects": [
+      {
+        "id": "1",
+        "title": "Parking App",
+        "ProgramingLanguage": "Flutter",
+        "body": "Crowdsourced mobile application to find parking for users.",
+        "link": "https://github.com/andrejdaskalov/parkingapp-mobile"
+      },
+      {
+        "id": "2",
+        "title": "ConcertTickets",
+        "body": "A Web Application for selling movie tickets using Onion Architecture",
+        "ProgramingLanguage": "C#",
+        "link": "https://github.com/GligorcoGligorov/ConcertTickets"
+      },
+      {
+        "title": "Up-Time",
+        "ProgramingLanguage": "Vue.js + Vercel",
+        "body": "Application for checking the availability/up-time of services mobile and web",
+        "link": "https://uptime-pearl.vercel.app",
+        "id": "3"
+      },
+      {
+        "title": "Tinex Store Chain",
+        "ProgramingLanguage": " Java Spring Boot + PostgreSql",
+        "body": "In-store product management and procurement and delivery of new products",
+        "link": "https://github.com/GligorcoGligorov/Tinex-Store-Chain",
+        "id": "4"
+      },
+      {
+        "title": "Book App",
+        "ProgramingLanguage": "Java Spring Boot + ReactJS",
+        "body": "lorem ipsum...",
+        "link": "https://github.com/GligorcoGligorov/Book-Application",
+        "id": "5"
+      },
+      {
+        "title": "Tank TroublESS",
+        "ProgramingLanguage": "Windows Forms + .NET",
+        "body": "The game is played with 2 players. They move through a maze, where they have to cleverly use the walls to hide from the enemy tank's bullets while managing to hit the enemy tank with their bullets",
+        "link": "https://github.com/GligorcoGligorov/TankTroublESS",
+        "id": "6"
+      }
+      
+    ],
+    "skills": [
+      "Java",
+      "Spring Framework",
+      "Spring MVC",
+      "Laboratory Skills",
+      "Web Development",
+      "Web Design",
+      "PostgreSQL",
+      "Software Development",
+      "Object-Oriented Programming (OOP)",
+      "Web Applications",
+      "Git",
+      "C (Programming Language)",
+      "jQuery",
+      "Responsive Web Design",
+      "Bootstrap (Framework)",
+      "JavaScript",
+      "HTML",
+      "Cascading Style Sheets (CSS)",
+      "ASP.NET MVC",
+      "ASP.NET Core",
+      "Django",
+      "C#",
+      "React.js",
+      "Python (Programming Language)",
+      "JSON",
+      "JUnit",
+      "Web Application Design",
+      "Back-End Web Development",
+      "Full-Stack Development",
+      "Kotlin",
+      "Hibernate",
+      "Java Frameworks",
+      "Microservices",
+      "REST APIs",
+      "Representational State Transfer (REST)",
+      "Code Review",
+      "C++",
+      "Agile Methodologies",
+      "English",
+      "SQL"
+    ],
+    "experiences": [
+      {
+        "name": "Laboratory Teaching Assistant Skills - Faculty of Computer Science and Engineering - Skopje",
+        "date": "Octomber 2023 - January 2024"
+      }
+    ],
+    "awards": [
+      {
+        "name": "EESTEC Academy Vue.js Training Certificate of Completion",
+        "link": "https://drive.google.com/file/d/1Us6BQKm9ybt3zhmXS1_mWPlT_RmL_RQG/view",
+        "date": "September 2021"
+      }
+    ]
+  }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,47 +1,59 @@
 
 import { useState, useEffect } from "react";
 
+import db_data from "../data/db";
 
 const useFetch = (url) => {
 
-    const [data,setData] = useState(null)
-    const [isPending, setIsPending] = useState(true)
-    const [error, setError] = useState(null)
+    const takeDataFor = url.split("http://localhost:8000/")[1];
+
+    let returnData = db_data[takeDataFor];
+    let takeDataForProject = 0;
+
+    if (takeDataFor.includes("projects/")) {
+        takeDataForProject = parseInt(takeDataFor.split("projects/")[1])-1;
+        returnData = db_data["projects"][takeDataForProject];
+    }
+
+    return {data: returnData, isPending: false, error: false}
+
+    // const [data,setData] = useState(null)
+    // const [isPending, setIsPending] = useState(true)
+    // const [error, setError] = useState(null)
 
 
+    // useEffect(() =>{
 
-    useEffect(() =>{
+    //     const abortCont = new AbortController();
 
-        const abortCont = new AbortController();
-
-        fetch(url, {signal: abortCont.signal})
-        .then(res => {
-            if(!res.ok){
-                throw Error("Could not fetch the data for that resourse")
-            }
-            return res.json()
-        })
-        .then(data => {
-            setData(data)
-            setIsPending(false)
-            setError(null)
-        })
-        .catch((err) => {
-            if(err.name === 'AbortError'){
-                console.log("fetch aborted")
-            }else{
-                setIsPending(false)
-                setError(err.message)
-            }
+    //     fetch(url, {signal: abortCont.signal})
+    //     .then(res => {
+    //         if(!res.ok){
+    //             throw Error("Could not fetch the data for that resourse")
+    //         }
+    //         return res.json()
+    //     })
+    //     .then(data => {
+    //         setData(data)
+    //         setIsPending(false)
+    //         setError(null)
+    //     })
+    //     .catch((err) => {
+    //         if(err.name === 'AbortError'){
+    //             console.log("fetch aborted")
+    //         }else{
+    //             setIsPending(false)
+    //             setError(err.message)
+    //         }
 
             
-        })
+    //     })
 
-        return () => abortCont.abort()
+    //     return () => abortCont.abort()
 
-    }, [url])
+    // }, [url])
 
-    return { data, isPending, error}
+    // return { data, isPending, error}
 }
 
 export default useFetch;
